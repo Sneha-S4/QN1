bddb9d59bb1d7241c9cc111d3ff09bcc9862df1e
Gayathri <l>
2023-11-14 17:51:04 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkAnswer() {
     var userAnswer = document.getElementById("answerInput").value.toLowerCase();
-    var correctAnswer = "echo";
+    var correctAnswer = "queue";
 
     if (userAnswer === correctAnswer) {
         alert("Congratulations! You got it right! Your next hint is at: ");
