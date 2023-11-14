5f4f5beea28eb913d23739fb3f78b70a736e127c
Gayathri <l>
2023-11-14 22:33:12 +0530

    initial commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     var correctAnswer = "queue";
 
     if (userAnswer === correctAnswer) {
-        alert("Congratulations! You got it right! Your next hint is at: above stage ");
+        alert("Congratulations! You got it right! Your next hint is at: above stage(cad lab) ");
         
     } else {
         alert("Sorry, that's not correct. Try again!");
