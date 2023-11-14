edf50430e255806d276d862f1860ce427e951c1a
Gayathri <l>
2023-11-14 22:22:12 +0530

    initial commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     var correctAnswer = "queue";
 
     if (userAnswer === correctAnswer) {
-        alert("Congratulations! You got it right! Your next hint is at: ");
+        alert("Congratulations! You got it right! Your next hint is at: above stage ");
         
     } else {
         alert("Sorry, that's not correct. Try again!");
